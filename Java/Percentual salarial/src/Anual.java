686d2e8db2fcfb90fccbac4a50fd3fff7b8c5a37
Abdoulayé Eduardo <abdoulayeduardo@gmail.com>
2024-10-23 14:25:33 +0100

    per

--- Java/Percentual salarial/src/Anual.java
+++ Java/Percentual salarial/src/Anual.java
@@ -1,27 +1,36 @@
 public class Anual {
     public static void main(String[] args) {
 
-        double sal_inicial=1000,percent=0.015,salAno;
+        double salary_inici =1000, percentage =0.015, salary_year;
         double dobro,cont=0;
-        int ano_inicial=2006
+        int year_inici =2005, year_act =2024;
 
         System.out.println("\nOlá senhor (a), abaixo está o relatórío do seu salário e dos aumentos de todos os anos que trabalhou na empesa.");
 
-        System.out.println("\nAno de "+ano_inicial+":");
-        System.out.println("Salario: "+sal_inicial+"0 R$");
-        System.out.print("Aumento: "+percent*100+"0%");
-        System.out.println("\n");
 
-        for (int i = 2007; i <=2024 ; i++) {
-                cont+=1;
-                dobro=percent*(Math.pow(2,cont));
 
-                salAno=(dobro*sal_inicial)+sal_inicial;
-                System.out.println("\nAno de "+i+": ");
-                System.out.println("Salario: "+salAno+"0 R$");
-                System.out.print("Aumento: "+dobro*100+"0%");
+        for (int i = year_inici; i <= year_act; i++) {
+
+            if(i==2005){
+                System.out.println("\nAno de "+i+":");
+                System.out.println("Salario: "+ salary_inici +"0 R$");
+                System.out.print("Aumento: 0%");
                 System.out.println("\n");
+            }else if(i==2006){
+                System.out.println("\nAno de "+i+":");
+                System.out.println("Salario: "+ salary_inici +"0 R$");
+                System.out.print("Aumento: "+ (percentage *salary_inici)+salary_inici+"0%");
+                System.out.println("\n");
+            }else {
+                cont += 1;
+                dobro = percentage * (Math.pow(2, cont));
 
+                salary_year = (dobro * salary_inici) + salary_inici;
+                System.out.println("\nAno de " + i + ": ");
+                System.out.println("Salario: " + salary_year + "0 R$");
+                System.out.print("Aumento: " + dobro * 100 + "0%");
+                System.out.println("\n");
+            }
         }
     }
 }
