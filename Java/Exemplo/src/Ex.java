584923f4d2536dc731167ca4ef5719e07ecc0075
Abdoulayé Eduardo <abdoulayeduardo@gmail.com>
2024-10-30 11:21:12 +0100

    foi feito alguns exercícios

---
+++ Java/Exemplo/src/Ex.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Ex {
+    public static final int numberIn=2;
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+
+
+    }
+}
